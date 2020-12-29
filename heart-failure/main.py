3a44a44b9a91d118ecfd10d54b26cee6bdab5fc2
alessiamarcolini <98marcolini@gmail.com>
2020-12-29 18:26:40 +0100

    Remove unused line

--- heart-failure/main.py
+++ heart-failure/main.py
@@ -5,8 +5,6 @@ from umap_scatter import get_umap
 
 data_provider = HeartFailureProvider("medical_data_embedding.csv")
 
-data_scr = data_provider.data_ds
-
 umap_scatter = get_umap(data_provider)
 
 curdoc().add_root(umap_scatter)
