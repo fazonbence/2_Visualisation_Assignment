74ce2b284afac83c4007e015930ebde4990d0dba
alessiamarcolini <98marcolini@gmail.com>
2021-01-02 16:21:00 +0100

    Show corresponding tiles near UMAP

--- heart-failure/data.py
+++ heart-failure/data.py
@@ -1,11 +1,30 @@
+import os
+
 import pandas as pd
 from bokeh.models import BooleanFilter, CDSView, ColumnDataSource
 
 
 class HeartFailureProvider:
-    def __init__(self, medical_data_path: str) -> None:
+    def __init__(
+        self,
+        medical_data_path: str,
+        imgs_folder: str = os.path.join("heart-failure", "static", "tiles_flat"),
+    ) -> None:
 
         self.medical_data = pd.read_csv(medical_data_path)
-        self.data_ds = ColumnDataSource(self.medical_data)
-        # self.data_view = CDSView(filters=[], source=self.data_ds)
+        self.filenames = self.medical_data["filename"].tolist()
+        self.patient_ids = self.medical_data["Patient Id"].tolist()
+        self.tile_ids = [f.split("_")[3] for f in self.filenames]
 
+        img_fmt = '<img src="{}/{}" ' 'alt="div_image" width="512" height="512">'
+
+        img_info = [
+            f"Patient ID: {p_id}, Tile ID: {t_id}"
+            for p_id, t_id in zip(self.patient_ids, self.tile_ids)
+        ]
+        self.medical_data["img_html"] = [
+            img_fmt.format(imgs_folder, x) for x in self.filenames
+        ]
+        self.medical_data["img_info"] = img_info
+
+        self.data_ds = ColumnDataSource(self.medical_data)
