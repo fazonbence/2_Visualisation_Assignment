8dbe8fb60814dfb0d4a6fee349bfb8120a5a0fc7
Bence Fazekas <fazon.bence@gmail.com>
2021-01-03 11:16:33 +0100

    Linking from q1_dotplot to umap works correctly
    However, from the another way it has a 1/11 chance to do something, due to the nature of the view

--- heart-failure/ourownfilters.py
+++ heart-failure/ourownfilters.py
@@ -16,6 +16,11 @@ Males = GroupFilter(column_name="Sex", group="male")
 #Default param should be: "data_provider.medical_data.size"
 UniqueIdBool =lambda length :  BooleanFilter([True if x%11==0 else False for x in range(0, length)])
 
+#Selects the remaining 10 row/patient
+#For linking purposes only!
+#Default param should be: "data_provider.medical_data.size"
+Inverse_UniqueIdBool =lambda length :  BooleanFilter([False if x%11==0 else True for x in range(0, length)])
+
 
 #Selects the rows with the same diagnosis type as the param
 DiagnosisType = lambda Type: GroupFilter(column_name="Diagnosis", group=Type);
