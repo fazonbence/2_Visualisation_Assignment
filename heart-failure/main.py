ccd1ca1209135ac08ca6372152da9d40ec847d68
Bence Fazekas <fazon.bence@gmail.com>
2021-01-03 10:57:47 +0100

    q1_dotplot linked w umap_scatter

--- heart-failure/main.py
+++ heart-failure/main.py
@@ -2,9 +2,20 @@ from bokeh.plotting import curdoc
 
 from data import HeartFailureProvider
 from umap_scatter import get_umap
+from q1_dotplot import get_q1dot
+from bokeh.layouts import gridplot
+from bokeh.plotting import figure, output_file, show
 
 data_provider = HeartFailureProvider("medical_data_embedding.csv")
 
 umap_scatter = get_umap(data_provider)
+#q1_dotplot = get_umap(data_provider)
+q1_dotplot = get_q1dot(data_provider)
 
-curdoc().add_root(umap_scatter)
+final_plot=gridplot([[umap_scatter, q1_dotplot]])
+
+#curdoc().add_root(final_plot)
+# output to static HTML file
+output_file("lines.html")
+show(final_plot)
+#curdoc().add_root(umap_scatter)
--- heart-failure/ourownfilters.py
+++ heart-failure/ourownfilters.py
@@ -1,4 +1,4 @@
-from bokeh.models import BooleanFilter, CDSView, ColumnDataSource, GroupFilter
+from bokeh.models import BooleanFilter, CDSView, ColumnDataSource, GroupFilter, CustomJSFilter
 
 
 
@@ -7,6 +7,14 @@ from bokeh.models import BooleanFilter, CDSView, ColumnDataSource, GroupFilter
 TrainingSet = GroupFilter(column_name="Dataset Name", group="training")
 #Selects the test data
 TrainingSet = GroupFilter(column_name="Dataset Name", group="test")
+
+#Selects females/males
+Females = GroupFilter(column_name="Sex", group="female")
+Males = GroupFilter(column_name="Sex", group="male")
+
+#Selects 1 row/patient
+#Default param should be: "data_provider.medical_data.size"
+UniqueIdBool =lambda length :  BooleanFilter([True if x%11==0 else False for x in range(0, length)])
 
 
 #Selects the rows with the same diagnosis type as the param
