052718215d6e974170346f2b0f20d620b4162a22
alessiamarcolini <98marcolini@gmail.com>
2021-01-05 14:04:55 +0100

    back to bokeh server and layout with HTML template

--- heart-failure/main.py
+++ heart-failure/main.py
@@ -1,8 +1,7 @@
 import os
 
 from bokeh.layouts import gridplot
-from bokeh.models import CustomJS, Div
-from bokeh.plotting import curdoc, figure, output_file, show
+from bokeh.plotting import curdoc, output_file, show
 
 from data import HeartFailureProvider
 from q1_dotplot import get_q1dot
@@ -13,18 +12,16 @@ data_provider = HeartFailureProvider(
 )
 
 umap_scatter, img, img_info = get_umap(data_provider)
-# q1_dotplot = get_umap(data_provider)
-q1_dotplot = get_q1dot(data_provider)
+mainPlot, Plot1, Plot2 = get_q1dot(data_provider)
 
-final_plot = gridplot([[umap_scatter, q1_dotplot, img, img_info]])
-
-# curdoc().add_root(final_plot)
 # output to static HTML file
-output_file("lines.html")
-show(final_plot)
-# curdoc().add_root(umap_scatter)
+# output_file("lines.html")
+# show(final_plot)
 
 
-# curdoc().add_root(umap_scatter)
-# curdoc().add_root(img)
-# curdoc().add_root(img_info)
+curdoc().add_root(umap_scatter)
+curdoc().add_root(img)
+curdoc().add_root(img_info)
+curdoc().add_root(mainPlot)
+curdoc().add_root(Plot1)
+curdoc().add_root(Plot2)
