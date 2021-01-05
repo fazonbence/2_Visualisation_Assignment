b28ff2143b3ae0021248376c07ded97a611e920c
alessiamarcolini <98marcolini@gmail.com>
2021-01-05 14:10:22 +0100

    fix typo in test dataset filter

--- heart-failure/ourownfilters.py
+++ heart-failure/ourownfilters.py
@@ -3,7 +3,7 @@ from bokeh.models import BooleanFilter, GroupFilter
 # Selects the training data
 TrainingSet = GroupFilter(column_name="Dataset Name", group="training")
 # Selects the test data
-TrainingSet = GroupFilter(column_name="Dataset Name", group="test")
+TestSet = GroupFilter(column_name="Dataset Name", group="test")
 
 # Selects females/males
 Females = GroupFilter(column_name="Sex", group="female")
