9a7bd90ab4236d9e882a5db802a75a031118224e
alessiamarcolini <98marcolini@gmail.com>
2021-02-06 19:25:20 +0100

    Move plotting modules in plots subpackage

--- heart-failure/main.py
+++ heart-failure/main.py
@@ -2,12 +2,12 @@ import os
 
 from bokeh.plotting import curdoc
 
-from clustered import get_clustered
 from control_panel import get_control_panel
 from data import HeartFailureProvider
-from dotplot import get_dot
-from stackedbar import get_bar
-from umap_scatter import get_umap
+from plots.clustered import get_clustered
+from plots.dotplot import get_dot
+from plots.stackedbar import get_bar
+from plots.umap_scatter import get_umap
 
 data_provider = HeartFailureProvider(
     "medical_data_embedding.csv", os.path.join("heart-failure", "static", "tiles_flat")
